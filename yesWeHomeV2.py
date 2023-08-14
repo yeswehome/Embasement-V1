e6159d05c9c22546172c2d467492b17a61c934a7
wawaiy <85480152+ilymsk@users.noreply.github.com>
2023-08-15 01:07:20 +0200

    Ajout constants.py et modifs main
    Ajout du fichier constants.py qui regroupe toutes les colonnes et l'éco part pour avoir un fichier plus lisible correction du font pour l'étirement du logo et la taille de la fenêtre principale

--- yesWeHomeV2.py
+++ yesWeHomeV2.py
@@ -3,223 +3,10 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import tkinter.ttk as ttk
 import pandas as pd
+import csv
 from PIL import Image, ImageTk
 
-# En-tête du fichier CSV pour les colonnes
-csv_header = "Caract_Collection;Produits_ReferenceFabriquant;Produits_Libelle;Caract_FamilleProduit;Produits_Description;Produits_Points;ProduitEcotaxe_Code_EcoMob3;ProduitEcotaxe_MontantHT_EcoMob3;Caract_Longueur;Caract_Hauteur;Caract_Largeur;Caract_PoidsNet\n"
-ECOPART_MAPPING = {
-0.04: "002",
-    0.08: "003",
-    0.13: "004",
-    0.17: "005",
-    0.33: "006",
-    0.83: "008",
-    1.25: "009",
-    1.67: "010",
-    2.08: "011",
-    2.5: "012",
-    2.92: "013",
-    3.33: "014",
-    3.75: "015",
-    4.17: "016",
-    5.42: "017",
-    5.83: "018",
-    6.67: "019",
-    7.5: "020",
-    8.33: "021",
-    10.83: "022",
-    15: "023",
-    19.17: "024",
-    27.5: "026",
-    31.67: "027",
-    40: "029",
-    52.5: "032",
-    69.17: "036",
-    5: "042",
-    0.68: "044",
-    0.02: "047",
-    0.05: "048",
-    0.09: "049",
-    0.15: "050",
-    0.21: "051",
-    0.5: "053",
-    0.92: "054",
-    0.96: "055",
-    1: "056",
-    1.42: "057",
-    1.46: "058",
-    1.5: "059",
-    1.92: "060",
-    2: "061",
-    2.33: "062",
-    4.58: "065",
-    7.08: "067",
-    7.92: "068",
-    8.75: "069",
-    9.17: "070",
-    12.5: "072",
-    17.5: "073",
-    21.67: "074",
-    26.67: "075",
-    51.67: "079",
-    61.67: "080",
-    76.67: "083",
-    86.67: "084",
-    106.67: "088",
-    0.1: "094",
-    0.03: "095",
-    0.06: "096",
-    0.12: "097",
-    0.14: "098",
-    0.16: "099",
-    0.18: "100",
-    0.2: "101",
-    0.25: "102",
-    0.27: "103",
-    0.31: "104",
-    0.4: "105",
-    0.46: "106",
-    0.47: "107",
-    0.49: "108",
-    0.51: "109",
-    0.67: "110",
-    0.69: "111",
-    0.81: "112",
-    1.08: "113",
-    1.21: "114",
-    1.33: "115",
-    1.71: "116",
-    1.83: "117",
-    2.25: "118",
-    2.67: "119",
-    2.75: "120",
-    3.08: "121",
-    3.25: "122",
-    3.58: "123",
-    3.67: "124",
-    3.92: "125",
-    4.42: "126",
-    4.5: "127",
-    4.67: "128",
-    4.75: "129",
-    4.83: "130",
-    5.17: "131",
-    5.33: "132",
-    5.58: "133",
-    6.08: "134",
-    6.25: "135",
-    6.92: "136",
-    8.83: "137",
-    10: "138",
-    10.42: "139",
-    11.25: "140",
-    11.67: "141",
-    13.33: "142",
-    14.17: "143",
-    15.42: "144",
-    16.25: "145",
-    17.08: "146",
-    18.33: "147",
-    20: "148",
-    20.83: "149",
-    22.5: "150",
-    25: "151",
-    26.92: "152",
-    30: "153",
-    30.83: "154",
-    31.25: "155",
-    32.17: "156",
-    32.5: "157",
-    34.17: "158",
-    35: "159",
-    35.42: "160",
-    37.42: "161",
-    37.5: "162",
-    39.17: "163",
-    42.5: "164",
-    42.67: "165",
-    42.92: "166",
-    43.33: "167",
-    43.75: "168",
-    45: "169",
-    45.42: "170",
-    47.5: "171",
-    47.92: "172",
-    50: "173",
-    50.42: "174",
-    53.17: "175",
-    55.42: "176",
-    55.83: "177",
-    56.25: "178",
-    58.42: "179",
-    60: "180",
-    60.42: "181",
-    62.5: "182",
-    63.67: "183",
-    64.17: "184",
-    65.42: "185",
-    68.33: "186",
-    68.75: "187",
-    68.92: "188",
-    72.5: "189",
-    72.92: "190",
-    74.17: "191",
-    75: "192",
-    75.42: "193",
-    77.92: "194",
-    79.42: "195",
-    80.83: "196",
-    81.25: "197",
-    84.17: "198",
-    84.67: "199",
-    85: "200",
-    85.42: "201",
-    87.5: "202",
-    89.92: "203",
-    93.75: "204",
-    95.17: "205",
-    95.42: "206",
-    100: "207",
-    100.42: "208",
-    104.17: "209",
-    105.42: "210",
-    105.67: "211",
-    106.25: "212",
-    110.92: "213",
-    112.5: "214",
-    112.92: "215",
-    115.42: "216",
-    117.92: "217",
-    118.75: "218",
-    121.67: "219",
-    125: "220",
-    125.42: "221",
-    129.17: "222",
-    130.42: "223",
-    131.25: "224",
-    135.42: "225",
-    139.17: "226",
-    140.42: "227",
-    145.42: "228",
-    147.92: "229",
-    151.67: "230",
-    155.42: "231",
-    156.67: "232",
-    162.92: "233",
-    165.42: "234",
-    # Notez que la valeur "165.42" est répétée, et elle sera écrasée dans un dictionnaire.
-    174.17: "236",
-    175.42: "237",
-    182.92: "238",
-    185.42: "239",
-    # Notez que la valeur "185.42" est répétée, et elle sera écrasée dans un dictionnaire.
-    195.42: "241",
-    196.67: "242",
-    205.42: "243",
-    207.92: "244",
-    219.17: "245",
-    230.42: "246",
-}
+from constants import csv_header, header_labels, COLONNE_MAPPING, ECOPART_MAPPING
 
 def validate_number(value):
     return value == "" or value.isdigit() or value.replace(".", "", 1).isdigit()
@@ -300,7 +87,63 @@ def add_to_existing_catalog():
     button_choose_csv = tk.Button(top, text="Choisir fichier", command=choose_csv_file)
     button_choose_csv.pack()
 
+def update_csv_header(filename, new_column_name):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    # Ajouter la nouvelle colonne à l'entête
+    header = lines[0].strip() + ";" + new_column_name + "\n"
+    lines[0] = header
+
+    # Réécrire le fichier avec la nouvelle entête
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+
+def get_additional_columns(filename):
+    additional_columns = []
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
+            # Supposer que les noms de colonne sont dans la première ligne
+            additional_columns = [col for col in row if col not in header_labels]
+            break
+    return additional_columns
+
+
+
 def create_data_entry(filename):
+    def add_new_column():
+        def add_column():
+            nonlocal row_count
+            new_column_name = new_column_entry.get().strip()
+            if new_column_name in COLONNE_MAPPING:
+                lbl = tk.Label(root, text=new_column_name)
+                lbl.grid(row=row_count, column=0)
+                entry = tk.Entry(root)
+                entry.grid(row=row_count, column=1)
+                entries.append(entry)
+                row_count += 1
+                button_save.grid(row=row_count, column=0)
+
+                update_csv_header(filename, new_column_name)
+                new_column_window.destroy()
+
+
+            else:
+                tk.messagebox.showerror("Erreur", "La valeur saisie ne correspond pas.")
+
+        new_column_window = tk.Toplevel(root)
+        new_column_window.title("Ajouter une colonne")
+
+        new_column_label = tk.Label(new_column_window, text="Nom de la colonne:")
+        new_column_label.pack()
+
+        new_column_entry = tk.Entry(new_column_window)
+        new_column_entry.pack()
+
+        button_add_column = tk.Button(new_column_window, text="Ajouter", command=add_column)
+        button_add_column.pack()
+
     def save_and_clear():
         save_to_csv(entries, filename)
         for entry in entries:
@@ -313,6 +156,7 @@ def create_data_entry(filename):
     root.geometry("800x600")
 
     entries = []
+    row_count = 0
 
     labels = ["Nom de la collection", "Référence du produit", "Libellé du produit", "Type de produit", "Description du produit", "Prix du produit", "Eco-part", "Longueur", "Largeur", "Hauteur", "Poids"]
     for i, label in enumerate(labels):
@@ -331,15 +175,32 @@ def create_data_entry(filename):
         entry.grid(row=i, column=1)
         entries.append(entry if label != "Type de produit" else type_produit_var)
 
+    row_count = len(labels)
+
+    # Ajout des colonnes supplémentaires depuis le fichier CSV
+    additional_columns = get_additional_columns(filename)
+    for col in additional_columns:
+        lbl = tk.Label(root, text=col)
+        lbl.grid(row=row_count, column=0)
+        entry = tk.Entry(root)
+        entry.grid(row=row_count, column=1)
+        entries.append(entry)
+        row_count += 1
+
+    button_add_column = tk.Button(root, text="+", command=add_new_column)
+    button_add_column.grid(row=row_count, column=2)
+
     button_save = tk.Button(root, text="Sauvegarder", command=save_and_clear)
-    button_save.grid(row=len(labels), column=0)
+    button_save.grid(row=row_count, column=0)
 
     root.mainloop()
+
+
 
 def main():
     root = tk.Tk()
     root.title("Gestionnaire de catalogues de meubles")
-    root.geometry("400x200")
+    root.geometry("800x400")
     root.configure(bg='white')  # Fond blanc pour l'application
 
     # Définir l'icône de la fenêtre
@@ -347,7 +208,7 @@ def main():
 
     # Charger le logo, le redimensionner et le placer au centre
     image = Image.open('logoYesWeHome.png')
-    image = image.resize((100, 100))  # Redimensionner l'image à 100x100
+    image = image.resize((281, 180))  # Redimensionner l'image à 100x100
     logo_image = ImageTk.PhotoImage(image)
     logo_label = tk.Label(root, image=logo_image, bg='white')  # Fond blanc pour le label
     logo_label.grid(row=0, column=0, columnspan=2)  # Centrer le logo
